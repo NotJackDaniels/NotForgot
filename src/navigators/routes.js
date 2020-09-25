c5fc38631068c77d095729cafd3e0c33b502a3ed
NotJackDaniels <danil2899@mail.ru>
2020-09-25 15:12:49 +0700

    small fix

--- src/navigators/routes.js
+++ src/navigators/routes.js
@@ -14,7 +14,7 @@ const AuthStack = createStackNavigator();
 export function AuthStackNavigator() {
     return(
         <AuthStack.Navigator screenOptions={{headerShown:false,}}>
-            {/* <AuthStack.Screen name={'Auth'} component={authLoadingScreen}/> */}
+            <AuthStack.Screen name={'Auth'} component={authLoadingScreen}/>
             <AuthStack.Screen name={'Login'} component={LoginScreen}/>
             <AuthStack.Screen name={'Registration'} component={RegistrationScreen}/>
             <AuthStack.Screen name={'MainPage'} component={MainScreen}/>
